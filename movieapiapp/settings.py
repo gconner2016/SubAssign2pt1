9d8d8423eb461e8529410075ba00cf4f72221146
gconner2016 <gconner@unomaha.edu>
2023-05-16 19:39:02 -0500

    update

--- movieapiapp/settings.py
+++ movieapiapp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mc#bk5&&a+f&#psh2l@ot_++_5f)n*m%#n_8=9l4vbmlf_cbpc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
